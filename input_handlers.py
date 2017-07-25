426da6f4c57c4cd77a6496c06e77ee4b613f0ea4
Robert Bond <robert@bryx.com>
2017-07-25 09:07:59 -0400

    Added vim key movement

--- input_handlers.py
+++ input_handlers.py
@@ -1,17 +1,17 @@
 def handle_keys(user_input):
-    if user_input.key == 'UP':
+    if user_input.key == 'UP' or (user_input.key == 'CHAR' and user_input.char == 'k'):
         return {
             'move': (0, -1)
         }
-    elif user_input.key == 'DOWN':
+    elif user_input.key == 'DOWN' or (user_input.key == 'CHAR' and user_input.char == 'j'):
         return {
             'move': (0, 1)
         }
-    elif user_input.key == 'LEFT':
+    elif user_input.key == 'LEFT' or (user_input.key == 'CHAR' and user_input.char == 'h'):
         return {
             'move': (-1, 0)
         }
-    elif user_input.key == 'RIGHT':
+    elif user_input.key == 'RIGHT' or (user_input.key == 'CHAR' and user_input.char == 'l'):
         return {
             'move': (1, 0)
         }
