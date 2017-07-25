e34a2a6a24b3512581d104faf6628e476b0dc95b
Robert Bond <robert@bryx.com>
2017-07-24 21:56:15 -0400

    Initial commit

---
+++ input_handlers.py
@@ -0,0 +1,28 @@
+def handle_keys(user_input):
+    if user_input.key == 'UP':
+        return {
+            'move': (0, -1)
+        }
+    elif user_input.key == 'DOWN':
+        return {
+            'move': (0, 1)
+        }
+    elif user_input.key == 'LEFT':
+        return {
+            'move': (-1, 0)
+        }
+    elif user_input.key == 'RIGHT':
+        return {
+            'move': (1, 0)
+        }
+
+    if user_input.key == 'ENTER' and user_input.alt:
+        return {
+            'fullscreen': True
+        }
+    elif user_input.key == 'ESCAPE':
+        return {
+            'exit': True
+        }
+
+    return {}
